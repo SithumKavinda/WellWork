139ac5d8cf2703b9f704e49d5be384709edc5149
Sithum kavinda <it20050344@my.sliit.lk>
2021-09-19 13:26:47 +0530

    Cart activity added

--- app/src/main/java/com/edu/wellwork/MainActivity.java
+++ app/src/main/java/com/edu/wellwork/MainActivity.java
@@ -2,13 +2,23 @@ package com.edu.wellwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfi"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openCart(View view){
+        Intent intent = new Intent(this, cart.class);
+        EditText
     }
 }
