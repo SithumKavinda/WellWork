ad6d167c277645d16cff5fca0d9962653f37ea46
Veesh <it20117832@my.sliit.lk>
2021-09-26 04:33:16 +0530

    RateUs page created

--- app/src/main/java/com/edu/wellwork/fragment_reviews.java
+++ app/src/main/java/com/edu/wellwork/fragment_reviews.java
@@ -1,19 +1,35 @@
 package com.edu.wellwork;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class fragment_reviews extends Fragment {
+    private Button button;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_reviews,container,false);
+        View view =  inflater.inflate(R.layout.fragment_reviews,container,false);
+        button = (Button) view.findViewById(R.id.buttonAddReview);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityAddReview();
+            }
+        });
+        return view;
+    }
+
+    public void openActivityAddReview() {
+        Intent intent = new Intent(getActivity(), AddReview.class);
+        startActivity(intent);
     }
 }
