e59b201ea9ec31b730f82432825ee230eb7fd675
Veesh <it20117832@my.sliit.lk>
2021-09-26 15:09:30 +0530

    Insert reviews

---
+++ app/src/main/java/com/edu/wellwork/ReviewDet.java
@@ -0,0 +1,25 @@
+package com.edu.wellwork;
+
+public class ReviewDet {
+    private String UserName;
+    private String UserReview;
+
+    public ReviewDet() {
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getUserReview() {
+        return UserReview;
+    }
+
+    public void setUserReview(String userReview) {
+        UserReview = userReview;
+    }
+}
