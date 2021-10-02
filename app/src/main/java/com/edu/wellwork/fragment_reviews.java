d5ded62fff586c61091013a301ff24a940395268
Veesh <it20117832@my.sliit.lk>
2021-10-03 02:20:37 +0530

    Custome dialog box added

--- app/src/main/java/com/edu/wellwork/fragment_reviews.java
+++ app/src/main/java/com/edu/wellwork/fragment_reviews.java
@@ -1,6 +1,9 @@
 package com.edu.wellwork;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,6 +27,7 @@ import java.util.ArrayList;
 
 public class fragment_reviews extends Fragment {
     private FloatingActionButton fabbtn;
+    private Dialog dialog;
 
     //DatabaseReference databaseReference;
 
@@ -33,6 +37,14 @@ public class fragment_reviews extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_reviews,container,false);
 
         //databaseReference = FirebaseDatabase.getInstance().getReference("ReviewDet");
+
+        Intent intent = new Intent(getActivity(), fragment_reviews.class);
+        dialog.setContentView(R.layout.addreview_dialog);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.background));
+        }
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(false);
 
         fabbtn = (FloatingActionButton) view.findViewById(R.id.fab_btn);
         fabbtn.setOnClickListener(new View.OnClickListener() {
