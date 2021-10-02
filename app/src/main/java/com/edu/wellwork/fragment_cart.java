3c450bcf2c6b2659c4f8af541bdda1bea0ec8014
User <User@DESKTOP-DDIU1MG>
2021-10-02 14:12:01 +0530

    Added navifation to payment

--- app/src/main/java/com/edu/wellwork/fragment_cart.java
+++ app/src/main/java/com/edu/wellwork/fragment_cart.java
@@ -1,9 +1,11 @@
 package com.edu.wellwork;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +16,17 @@ public class fragment_cart extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_cart,container,false);
+        View view = inflater.inflate(R.layout.fragment_cart, container, false);
+        onButtonClicked(view);
+        return view;
+    }
+
+    protected void onButtonClicked(View view){
+        System.out.println("CLICKED");
+        Button cardButton = (Button)view.findViewById(R.id.pay);
+        cardButton.setOnClickListener(view1 -> {
+            Intent cardActivity = new Intent(view.getContext(), CardManagment.class);
+            startActivity(cardActivity);
+        });
     }
 }
