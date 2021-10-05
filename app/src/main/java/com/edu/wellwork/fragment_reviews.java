380c10c5eee6acba4ec7d870c58a15cca4a8b607
Veesh <it20117832@my.sliit.lk>
2021-10-05 12:58:49 +0530

    UI created

--- app/src/main/java/com/edu/wellwork/fragment_reviews.java
+++ app/src/main/java/com/edu/wellwork/fragment_reviews.java
@@ -26,38 +26,24 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class fragment_reviews extends Fragment {
-    private FloatingActionButton fabbtn;
-    private Dialog dialog;
 
-    //DatabaseReference databaseReference;
+    public FloatingActionButton floatingActionButton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_reviews,container,false);
 
-        //databaseReference = FirebaseDatabase.getInstance().getReference("ReviewDet");
+        floatingActionButton = (FloatingActionButton) view.findViewById(R.id.fab_btn);
 
-        Intent intent = new Intent(getActivity(), fragment_reviews.class);
-        dialog.setContentView(R.layout.addreview_dialog);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
-            //dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.background));
-        }
-        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
-        dialog.setCancelable(false);
-
-        fabbtn = (FloatingActionButton) view.findViewById(R.id.fab_btn);
-        fabbtn.setOnClickListener(new View.OnClickListener() {
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openActivityAddReview();
+                Intent intent = new Intent(getActivity(),write_review.class);
+                startActivity(intent);
             }
         });
         return view;
+        };
     }
 
-    public void openActivityAddReview() {
-        Intent intent = new Intent(getActivity(), AddReview.class);
-        startActivity(intent);
-    }
-}
